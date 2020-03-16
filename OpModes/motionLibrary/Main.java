7f93b17a1695eaa24bb20c74eda1348b7161a2aa
kwobny <ykwon23@madisonnjps.org>
2020-03-16 03:08:27 +0000

    Accidental auto-format

--- OpModes/motionLibrary/Main.java
+++ OpModes/motionLibrary/Main.java
@@ -4,7 +4,7 @@ import org.firstinspires.ftc.teamcode.Other.Backend.MadHardware;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Main {
-  //sub objects
+  // sub objects
   public MadHardware mhw;
 
   public WaitConditionClass waitConditions = new WaitConditionClass();
@@ -18,15 +18,16 @@ public class Main {
     mhw = hmw;
   }
 
-  //loop for the motions
-  //allows movements which are time dependent, like rotational translation
-  //also allows execution of other stuff
+  // loop for the motions
+  // allows movements which are time dependent, like rotational translation
+  // also allows execution of other stuff
   public void loop() {
     wait.executeIntervals();
     wait.runTimeouts();
   }
 
-  //function that you call at the end of autonomous sequence to wait for program to end. This should be the absolute last function executed in program.
+  // function that you call at the end of autonomous sequence to wait for program
+  // to end. This should be the absolute last function executed in program.
   public void endProgram() {
     while (true) {
       loop();
