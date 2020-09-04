662ab1b99575016682afa944d384fff617089590
Yeongjin Kwon <ykwon23@madisonnjps.org>
2020-09-04 14:28:41 +0000

    Changed the Wait Task class to accommodate multiple callbacks and added the end task after flag. The Wait Task class is becoming more accessible to the user.

--- OpModes/motionLibrary/Wait_Code/WaitTask.java
+++ OpModes/motionLibrary/Wait_Code/WaitTask.java
@@ -5,18 +5,25 @@ package org.firstinspires.ftc.teamcode.OpModes.motionLibrary;
 
 public class WaitTask {
   public WaitCondition condition;
-  public Callback callback;
+  public Callback[] callbacks;
   public Callback runWhile; //this is basically an optional callback that continuously runs in the main loop while the wait is ongoing.
   //It is run after the wait condition is tested, so if the wait is determined to be over, then this does not run.
+
+  public boolean endTaskAfter = true;
+  //is a property that specifies whether or not to end the wait after the condition has been met.
 
   //A return value of true means that the wait condition has been satisfied and the wait is over. A return value of false means that the wait is still ongoing.
   boolean run() {
 
     if (condition.pollCondition()) {
-      if (callback != null) {
-        callback.run();
+      if (callbacks != null) {
+        for (Callback i : callbacks) {
+          if (i != null)
+            i.run();
+        }
       }
-      return true;
+      if (endWait)
+        return true;
     }
     if (runWhile != null) {
       runWhile.run();
@@ -25,12 +32,32 @@ public class WaitTask {
 
   }
 
-  public WaitTask(final WaitCondition condition, final Callback callback, final Callback runWhile) throws Exception {
+  public void setTask(final WaitCondition cond, final Callback ...callbacks) {
+    setWait(cond);
+    setCallback(callbacks);
+  }
+  public void setWait(final WaitCondition cond) throws Exception {
+    if (cond == null) {
+      throw new Exception("wait condition cannot be null");
+    }
+    condition = cond;
+  }
+  public void setCallback(final Callback ...callbacks) {
+    this.callbacks = callbacks;
+  }
+
+  public WaitTask(final WaitCondition condition, final Callback runWhile, final Callback[] callbacks) throws Exception {
     if (condition == null) {
       throw new Exception("There is no WaitCondition provided");
     }
     this.condition = condition;
-    this.callback = callback;
+    this.callbacks = callbacks;
     this.runWhile = runWhile;
+  }
+  public WaitTask(final WaitCondition condition, final Callback callback, final Callback runWhile) {
+    this(condition, runWhile, new Callback[]{callback});
+  }
+  public WaitTask() {
+    //
   }
 }
