b580c25a70fe01aa234782b8945fe11fc52b3c96
Yeongjin Kwon <ykwon23@madisonnjps.org>
2020-09-18 05:06:59 +0000

    I'm thinking of scrapping the cancellable callback system Made some changes to cancellable callback system

--- OpModes/motionLibrary/Wait_Code/CancelCallback/CCConsumer.java
+++ OpModes/motionLibrary/Wait_Code/CancelCallback/CCConsumer.java
@@ -7,23 +7,27 @@ public abstract class CCConsumer<T extends CancellableCallback> {
   //these methods should be PRIVATE, but are protected because they need to be accessible and overridable from any subclass
   protected abstract void _actualRun(T obj);
 
-  //protected abstract void _removeCallback(T obj);
+  protected abstract void _removeCallback(T obj);
 
   //protected abstract void _addCallback(T obj);
 
   //these methods are public because the consumer object itself is likely to be private.
-  public final void run(T obj) {
+  public final void run(CancellableCallback obj) {
     if (obj.isActive)
       _actualRun(obj);
+    else {
+      markAsRemoved(obj);
+      _removeCallback(obj);
+    }
   }
 
-  public final void markAsAdded(T obj) throws Exception {
+  public final void markAsAdded(CancellableCallback obj) throws Exception {
     if (obj.isActive)
       throw new Exception("You cannot add a cancellable callback that is already added.");
     obj.isActive = true;
   }
 
-  public final void markAsDeleted(T obj) throws Exception {
+  public final void markAsRemoved(CancellableCallback obj) throws Exception {
     if (!obj.isActive)
       throw new Exception("You cannot delete a cancellable callback that has not been added yet.");
     obj.isActive = false;
