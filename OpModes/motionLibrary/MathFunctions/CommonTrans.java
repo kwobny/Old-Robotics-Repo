a3015cddbc583e7153d5678d16cc93f8f4d67f6f
Yeongjin Kwon <ykwon23@madisonnjps.org>
2020-09-20 04:55:28 +0000

    Refactored common transformations

--- OpModes/motionLibrary/MathFunctions/CommonTrans.java
+++ OpModes/motionLibrary/MathFunctions/CommonTrans.java
@@ -2,16 +2,14 @@ package org.firstinspires.ftc.teamcode.OpModes.motionLibrary.MathFunctions;
 
 public class CommonTrans {
   
-  public static class Translate extends Transformation {
+  public static class TranslateX extends Transformation {
 
     public double shiftX;
-    public double shiftY;
 
-    public Translate(final double shiftX, final double shiftY) {
+    public TranslateX(final double shiftX) {
       this.shiftX = shiftX;
-      this.shiftY = shiftY;
     }
-    public Translate() {
+    public TranslateX() {
       //
     }
 
@@ -19,34 +17,56 @@ public class CommonTrans {
     public double alteredX(final double input) {
       return input - shiftX;
     }
+  }
+
+  public static class TranslateY extends Transformation {
+
+    public double shiftY;
+
+    public TranslateY(final double shiftY) {
+      this.shiftY = shiftY;
+    }
+    public TranslateY() {
+      //
+    }
 
     @Override
-    public double alteredY(final double output) {
-      return output + shiftY;
+    public double alteredY(final double input) {
+      return input + shiftY;
     }
   }
 
-  public static class Scale extends Transformation {
+  public static class ScaleX extends Transformation {
 
-    public double scaleX;
-    public double scaleY;
+    public double factorX;
 
-    public Scale(final double scaleX, final double scaleY) {
-      this.scaleX = scaleX;
-      this.scaleY = scaleY;
+    public ScaleX(final double factorX) {
+      this.factorX = factorX;
     }
-    public Scale() {
+    public ScaleX() {
       //
     }
 
     @Override
     public double alteredX(final double input) {
-      return input/scaleX;
+      return input/factorX;
+    }
+  }
+
+  public static class ScaleY extends Transformation {
+
+    public double factorY;
+
+    public ScaleY(final double factorY) {
+      this.factorY = factorY;
+    }
+    public ScaleY() {
+      //
     }
 
     @Override
-    public double alteredY(final double output) {
-      return output * scaleY;
+    public double alteredY(final double input) {
+      return input * factorY;
     }
   }
 
