2732f45ca8757824df86c504cb4e07bce5b6072c
Yeongjin Kwon <ykwon23@madisonnjps.org>
2020-09-25 13:35:54 +0000

    Created a new motion profiles class to implement seq op unit

---
+++ OpModes/motionLibrary/AdvancedImplementations/MotionProfiles2.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.OpModes.motionLibrary.*;
+
+import org.firstinspires.ftc.teamcode.OpModes.motionLibrary.Wait_Package.*;
+import org.firstinspires.ftc.teamcode.OpModes.motionLibrary.SCS_Package.*;
+
+public class MotionProfiles2 {
+
+  public WaitCore waitCore;
+  public SCS scs;
+
+  public MotionProfiles2() {
+    //
+  }
+  public MotionProfiles2(final WaitCore core, final SCS scs) {
+    this.waitCore = core;
+    this.scs = scs;
+  }
+
+  public class 
+
+}
