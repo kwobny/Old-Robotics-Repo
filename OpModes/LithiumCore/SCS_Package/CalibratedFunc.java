6075908145163c961e030b7b3744303c7b0e3f36
Yeongjin Kwon <ykwon23@madisonnjps.org>
2020-10-29 18:39:06 +0000

    Renamed calibratable func to calibrated func

--- OpModes/LithiumCore/SCS_Package/CalibratedFunc.java
+++ OpModes/LithiumCore/SCS_Package/CalibratedFunc.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.OpModes.LithiumCore.SCS_Package;
 import org.firstinspires.ftc.teamcode.OpModes.LithiumCore.Utils.MathFunctions.*;
 
 //This interface denotes that a function needs to be auto calibrated when used.
-public interface CalibratableFunc extends MathFunction {
+public interface CalibratedFunc extends MathFunction {
   
   //
   
