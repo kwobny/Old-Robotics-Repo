ec9e54551a00586eb3ba422a80353bf948766ca3
Yeongjin Kwon <ykwon23@madisonnjps.org>
2020-11-02 14:17:58 +0000

    Added a parameter container functionality

--- OpModes/LithiumCore/AdvancedImplementations/SigmoidFunc.java
+++ OpModes/LithiumCore/AdvancedImplementations/SigmoidFunc.java
@@ -10,20 +10,51 @@ public class SigmoidFunc implements MathFunction {
   private double kNeg; //negative of the logistic growth rate
   private double x0; //midpoint of curve
 
+  //constructors
   public SigmoidFunc(final double L, final double k, final double x0) {
     setParams(L, k, x0);
+  }
+  public SigmoidFunc(final ParamContainer params) {
+    setParams(params);
   }
   protected SigmoidFunc() {
     //
   }
+
+  //set parameters function
   protected void setParams(final double L, final double k, final double x0) {
     this.L = L;
     this.kNeg = -k;
     this.x0 = x0;
+  }
+  protected void setParams(final ParamContainer p) {
+    setParams(p.L, p.k, p.x0);
   }
 
   @Override
   public double yValueOf(final double x) {
     return L/(1 + Math.exp(kNeg * (x - x0)));
   }
+
+  //Return the parameters of the sigmoid func in the form of a parameter container.
+  public ParamContainer getParams() {
+    return new ParamContainer(L, -kNeg, x0);
+  }
+
+  //This is basically a container that contains all of the parameters for the sigmoid function.
+  public static class ParamContainer {
+    public double L;
+    public double k;
+    public double x0;
+
+    public ParamContainer() {
+      //
+    }
+    public ParamContainer(final double L, final double k, final double x0) {
+      this.L = L;
+      this.k = k;
+      this.x0 = x0;
+    }
+  }
+
 }
