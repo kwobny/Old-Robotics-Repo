3d7876af3abba2a500b4ecebdebde4a074dab3fe
Yeongjin Kwon <ykwon23@madisonnjps.org>
2021-02-03 13:47:08 +0000

    Created a method to drive for a certain distance.

--- OpModes/Auto2020.java
+++ OpModes/Auto2020.java
@@ -6,14 +6,19 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.Backend.MadHardware;
 import org.firstinspires.ftc.teamcode.LithiumCore.*;
 import org.firstinspires.ftc.teamcode.LithiumCore.SharedState.*;
+import org.firstinspires.ftc.teamcode.LithiumCore.Utils.Vector;
 
 @Autonomous(name = "Yeongjin's autonomous \"yay!\"", group = "Autos")   // How opmode is displayed on phones
 public class Auto2020 extends LinearOpMode
 {
 
-    // Initializing the motor-control class.
+    // Initializing the motor-control class and other constants.
     public static MadHardware mhw = new MadHardware();
-    //public static ElapsedTime runtime = new ElapsedTime();
+    final static Main robotLib = new Main(mhw, new MadMachinesConstants());
+    
+    // Init constants.
+    // This variable defines the ratio between robot speed in units per second vs. power applied.
+    private static double speedPerPower = "need to set this parameter";
 
     // This method is run once when the "INIT" button is pressed on the phone.
     @Override
@@ -25,8 +30,6 @@ public class Auto2020 extends LinearOpMode
         }
 
         mhw.initHardware(hardwareMap);
-
-        final Main robotLib = new Main(mhw, new MadMachinesConstants());
 
         waitForStart();
 
@@ -41,5 +44,15 @@ public class Auto2020 extends LinearOpMode
         robotLib.move.syncMotors();
 
         robotLib.endAutonomous();
+    }
+
+    // This method takes a power vector represented by two vector components
+    // And a distance parameter in whatever unit the robot distances are measured in (I think centimeters).
+    private void translateDistance(double px, double py, double distance) {
+        // Speed in units per second
+        double speed = Vector.length(px, py) * speedPerPower;
+        double waitTime = distance/speed;
+        robotLib.translate(px, py);
+        robotLib.wait.waitFor(robotLib.time.getWait(waitTime).start());
     }
 } // End of class
