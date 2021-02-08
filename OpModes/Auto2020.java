a5f6b15f0d4ceaf592fb51c55b44bd2416c2357e
repl.it user <replituser@example.com>
2021-02-08 08:26:42 +0000

    Made a drive forward for distance method, and integrated it into the autonomous routine.
    need to find what distance robot needs to travel.

--- OpModes/Auto2020.java
+++ OpModes/Auto2020.java
@@ -7,18 +7,16 @@ import org.firstinspires.ftc.teamcode.Backend.MadHardware;
 import org.firstinspires.ftc.teamcode.LithiumCore.*;
 import org.firstinspires.ftc.teamcode.LithiumCore.SharedState.*;
 import org.firstinspires.ftc.teamcode.LithiumCore.Utils.Vector;
+import org.firstinspires.ftc.teamcode.LithiumCore.Wait_Package.WaitCondition;
 
 @Autonomous(name = "Yeongjin's autonomous \"yay!\"", group = "Autos")   // How opmode is displayed on phones
 public class Auto2020 extends LinearOpMode
 {
 
     // Initializing the motor-control class and other constants.
-    public static MadHardware mhw = new MadHardware();
-    final static Main robotLib = new Main(mhw, new MadMachinesConstants());
-    
-    // Init constants.
-    // This variable defines the ratio between robot speed in units per second vs. power applied.
-    private static double speedPerPower = "need to set this parameter";
+    private static final MadHardware mhw = new MadHardware();
+    private static final MadMachinesConstants constants = new MadMachinesConstants();
+    private static final Main robotLib = new Main(mhw, constants);
 
     // This method is run once when the "INIT" button is pressed on the phone.
     @Override
@@ -35,10 +33,8 @@ public class Auto2020 extends LinearOpMode
 
         robotLib.startAutonomous();
 
-        robotLib.move.translate(0, 1);
-        robotLib.move.syncMotors();
-
-        robotLib.wait.waitFor(robotLib.time.getWait(3.14159265).start());
+        // Input distance in centimeters.
+        driveForward(1.0, distance);
 
         robotLib.move.clearAll();
         robotLib.move.syncMotors();
@@ -46,13 +42,43 @@ public class Auto2020 extends LinearOpMode
         robotLib.endAutonomous();
     }
 
-    // This method takes a power vector represented by two vector components
+    // Drives forward or backward at the provided power for a certain distance. Does not stop after drive is over.
+    // This is a blocking method.
+    // This method takes a power vector. Forward is positive, backward is negative.
     // And a distance parameter in whatever unit the robot distances are measured in (I think centimeters).
-    private void translateDistance(double px, double py, double distance) {
-        // Speed in units per second
-        double speed = Vector.length(px, py) * speedPerPower;
-        double waitTime = distance/speed;
-        robotLib.translate(px, py);
-        robotLib.wait.waitFor(robotLib.time.getWait(waitTime).start());
+    // Power can be positive or negative,
+    // distance is always positive.
+    private void driveForward(double power, double distance) {
+        final int distanceInTicks = Math.round(distance/constants.robotParameters.distancePerTick);
+        // Left front, right front, left rear, right rear
+        final int[] initialPositions = {
+            mhw.leftFront.getCurrentPosition(),
+            mhw.rightFront.getCurrentPosition(),
+            mhw.leftRear.getCurrentPosition(),
+            mhw.rightRear.getCurrentPosition()
+        };
+
+        robotLib.translate(0.0, power);
+        robotLib.syncMotors();
+
+        robotLib.wait.waitFor(new WaitCondition() {
+            @Override
+            public boolean pollCondition() {
+                final double leftFrontDelta = mhw.leftFront.getCurrentPosition() - initialPositions[0];
+                final double rightFrontDelta = mhw.rightFront.getCurrentPosition() - initialPositions[1];
+                final double leftRearDelta = mhw.leftRear.getCurrentPosition() - initialPositions[2];
+                final double rightRearDelta = mhw.rightRear.getCurrentPosition() - initialPositions[3];
+
+                final double averageTicksTraveled
+                = Math.abs(
+                    leftFrontDelta/4
+                    + rightFrontDelta/4
+                    + leftRearDelta/4
+                    + rightRearDelta/4
+                );
+
+                return averageTicksTraveled >= distanceInTicks;
+            }
+        });
     }
 } // End of class
