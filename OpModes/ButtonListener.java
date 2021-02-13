205c2f88d291b0a9b52811c43d2a1e05b5e9086e
yeongjin's surface pro <yeongjinkwon@gmail.com>
2021-02-12 22:16:57 -0500

    Refactored button listener to fit toggle listener
    Also optimized stuff in toggle listener

--- OpModes/ButtonListener.java
+++ OpModes/ButtonListener.java
@@ -1,29 +1,18 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
-import org.firstinspires.ftc.teamcode.LithiumCore.Utils.BooleanConsumer;
-
 // This class is a version of a toggle listener that listens specifically
 // for when the boolean transitions to a specific state (the trigger state).
 public class ButtonListener extends ToggleListener {
-    public ButtonListener() {
-        //
-    }
-    public ButtonListener(final BooleanConsumer callback) {
-        super(callback);
-    }
 
-    // The trigger state is the value when the callback should be called.
+    // The trigger state is the value when the listener should indicate that
+    // The value has changed.
     // By default, it is false, which means that the callback triggers
     // when the boolean goes from true to false (button is pressed to not pressed).
-    private boolean triggerState = false;
-
-    // The method used to set the trigger state.
-    public void setTriggerState(final boolean value) {
-        triggerState = value;
-    }
-
+    public boolean triggerState = false;
+    
+    // Returns true when the value shifts to the trigger state.
     @Override
-    protected void onToggle(final boolean value) {
-        if (value == triggerState) callback.accept(value);
+    public boolean set(final boolean value) {
+        return super.set(value) && value == triggerState;
     }
 }
--- OpModes/ToggleListener.java
+++ OpModes/ToggleListener.java
@@ -1,16 +1,10 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
-import org.firstinspires.ftc.teamcode.LithiumCore.Utils.BooleanConsumer;
-
 // This is a listener that listens for whenever a boolean value is toggled.
 // You continually provide a boolean into the listener and when the boolean transitions states, the consumer is called.
 public class ToggleListener {
 
     private boolean value = false; //default value at beginning is false.
-
-    public ToggleListener() {
-        //
-    }
 
     // Sets the current value.
     // Returns true if a value that is opposite from the current value is inputted,
