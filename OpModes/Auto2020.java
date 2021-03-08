27cff2b6f00e39f0a9e4d02ae5242c7226d2528e
Robotics computer <ykwon23@madisonnjps.org>
2021-03-08 14:34:57 -0500

    Created 2 things
    Makeshift distance mechanics
    Test to determine distance constant

--- OpModes/Auto2020.java
+++ OpModes/Auto2020.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.Backend.MadHardware;
 
 import org.firstinspires.ftc.teamcode.LithiumCore.Main;
+package org.firstinspires.ftc.teamcode.LithiumCore.Utils.Vector;
 
 @Autonomous(name = "Yeongjin's autonomous \"yay!\"", group = "Autos")   // How opmode is displayed on phones
 public class Auto2020 extends LinearOpMode
@@ -13,6 +14,19 @@ public class Auto2020 extends LinearOpMode
 
     // Initializing the motor-control class.
     public static MadHardware mhw = new MadHardware();
+    
+    // The velocity (meters/second) traveled per power (no units).
+    // dx/(dt*power)
+    final double velocityPerPower = 0.3;
+    private void driveDistance(double rx, double ry, double distance) {
+        robotLib.move.translate(rx, ry);
+        robotLib.move.syncMotors();
+
+        robotLib.wait.waitFor(robotLib.time.getWait(distance/(velocityPerPower*Vector.length(rx, ry))).start());
+
+        robotLib.move.clearAll();
+        robotLib.move.syncMotors();
+    }
     
     // This method is run once when the "INIT" button is pressed on the phone.
     @Override
@@ -29,7 +43,7 @@ public class Auto2020 extends LinearOpMode
         robotLib.move.translate(0, 1);
         robotLib.move.syncMotors();
 
-        robotLib.wait.waitFor(robotLib.time.getWait(3.14159265).start());
+        robotLib.wait.waitFor(robotLib.time.getWait(5.0).start());
 
         robotLib.move.clearAll();
         robotLib.move.syncMotors();
