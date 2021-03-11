a43fe6ca0395cbecbb61baaaf017d0f8a642aac0
Robotics computer <ykwon23@madisonnjps.org>
2021-03-11 14:26:29 -0500

    made test an actual working test.

--- OpModes/ServoTest.java
+++ OpModes/ServoTest.java
@@ -1,37 +1,61 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import android.widget.Button;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.Backend.MadHardware;
-
+import org.firstinspires.ftc.teamcode.Backend.Notifier;
 import org.firstinspires.ftc.teamcode.LithiumCore.Main;
+import org.firstinspires.ftc.teamcode.LithiumCore.Utils.Vector;
 
-@Autonomous(name = "Servo test", group = "Autos")   // How opmode is displayed on phones
-public class Auto2020 extends LinearOpMode
-{
+@TeleOp(name = "Servo test", group = "TeleOp")
+public class ServoTest extends OpMode {
 
-    // Initializing the motor-control class.
-    public static MadHardware mhw = new MadHardware();
-    
-    // This method is run once when the "INIT" button is pressed on the phone.
+    MadHardware mhw = new MadHardware();
+    Notifier noto = new Notifier(telemetry);
+    //ElapsedTime runtime = new ElapsedTime();
+
+    final Main robotLib = new Main(mhw, new MadMachinesConstants());
+
     @Override
-    public void runOpMode()
-    {
+    public void init() {
+        noto.cMessage("Initialization starting.");
         mhw.initHardware(hardwareMap);
-
-        final Main robotLib = new Main(mhw, new MadMachinesConstants());
-
-        waitForStart();
-
-        robotLib.startAutonomous();
-
-        for (double i : new double[]{0.25, -0.5, 0.75, -1.0}) {
-            mhw.tempServo.setPower(i);
-            robotLib.wait.waitFor(robotLib.time.getWait(2).start());
-        }
-        mhw.tempServo.setPower(0.0);
-
-        robotLib.endAutonomous();
+        noto.message("Initialization complete.");
     }
-} // End of class
+
+    @Override
+    public void start() {
+        noto.message("Teleop starting.");
+
+        //start stuff
+        robotLib.startTeleOp();
+        mhw.tempServo.setPower(0.5);
+
+        noto.message("Teleop start complete.");
+    }
+
+    private ButtonListener listener = new ButtonListener();
+    private double[] powerList = {0.0, 0.25, 0.5, 0.75, 1.0};
+    private int index = 0;
+
+    //The main driving code should never directly interface with controllers
+    @Override
+    public void loop() {
+        //noto.cMessage("Main teleop loop running.");
+        // loop method code
+        //gamepad1 is the main driver, gamepad2 is auxiliary driver.
+
+        robotLib.loopAtBeginning();
+
+        if (listener.set(gamepad1.x)) {
+            mhw.tempServo.setPower(powerList[index++]);
+        }
+
+        robotLib.loopAtEnd();
+    }
+
+}
